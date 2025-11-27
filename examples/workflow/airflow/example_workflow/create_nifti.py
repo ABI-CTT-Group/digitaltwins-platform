3db34f5d2b0ec85a1d2d9f9cdd859a42286a5179
Chinchien <LIN810116@gmail.com>
2025-11-27 23:27:28 +1300

    chore [airflow]: add example workflow/dag

---
+++ examples/workflow/airflow/example_workflow/create_nifti.py
@@ -0,0 +1,24 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+
+
+def add_to_dataset(assay_seek_id, workspace, platform_config_file):
+    import time
+    time.sleep(3)
+
+
+def exec(**kwargs):
+    assay_seek_id = None
+    workspace = None
+    platform_config_file = None
+
+    add_to_dataset(assay_seek_id=assay_seek_id, workspace=workspace, platform_config_file=platform_config_file)
+
+
+def get_task(dag: DAG):
+    task_id = "create_nifti"
+    return PythonOperator(
+        task_id=task_id,
+        python_callable=exec,
+        dag=dag
+    )
