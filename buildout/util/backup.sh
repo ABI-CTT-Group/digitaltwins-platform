202aabef4cb7e658203d05ca8a3af430641457cb
rocky Cloud User <matt.pestle@nesi.org.nz>
2025-12-12 09:34:44 +1300

    add backup.sh

--- buildout/util/backup.sh
+++ buildout/util/backup.sh
@@ -1,22 +1,48 @@
-# Improve this, but this is the shell of what needs to happen
+# Backup the docker volumes
+#
+
+export TZ=Pacific/Auckland
+LOG_DIR=${LOG_DIR:=~/logs}
+BACKUP_DIR=${BACKUP_DIR:=~/backups}
+
+[[ -d $LOG_DIR ]] || {
+    mkdir $LOG_DIR || exit 1
+}
+[[ -d $BACKUP_DIR ]] || {
+    mkdir $BACKUP_DIR || exit 1
+}
+now=$(date +%Y%m%d%H%M%S)
+
+LOG_FILE=$LOG_DIR/backup_$now.log
+
+BACKUP_FILE=${BACKUP_DIR}/docker_vols_${now}.tar.gz
+
+exec > $LOG_FILE 2>&1
+
+date
+
 
 # On the system to be copied
 docker compose down
 
 sudo su -  << EOF
-cd /var/lib/docker/volumes/; tar cvf /tmp/mp.tar digitaltwins*/_data
-EOF
+cd /var/lib/docker/volumes/; tar cvf $BACKUP_FILE digitaltwins*/_data
+EOF || {
+    echo "Error creating tar archive. Quitting"
+    exit 1
+}
 
 docker compose up -d
 
-# copy mp.tar to the backup system, then on that backup system
-
-sudo su - <<EOF
-cd /var/lib/docker/volumes/
-rm -rf digitaltwins*/_data
-tar xvf /tmp/mp.tar
-EOF
-
-# Some passwords will then need to be sorted out:
-# 1. services/seek/ldh-deployment/docker-compose.env - copy from prod to dev
-# 2. services/api/digitaltwins-api/configs.ini - the api token needs to be copied from prod
+#
+## copy mp.tar to the backup system, then on that backup system
+#
+#sudo su - <<EOF
+#cd /var/lib/docker/volumes/
+#rm -rf digitaltwins*/_data
+#tar xvf /tmp/mp.tar
+#EOF
+#
+## Some passwords will then need to be sorted out:
+## 1. services/seek/ldh-deployment/docker-compose.env - copy from prod to dev
+## 2. services/api/digitaltwins-api/configs.ini - the api token needs to be copied from prod
