249c1006413c0c36d43738d25144a41a4507add5
Matt Pestle <matt.pestle@nesi.org.nz>
2026-05-27 13:17:16 +1200

    syntax in ufw_for_walled_garden.sh script

--- buildout/util/ufw_for_walled_garden.sh
+++ buildout/util/ufw_for_walled_garden.sh
@@ -2,12 +2,12 @@ PUBLIC_FACING_IF=eth0
 WALLED_GARDEN_IF=eth1
 WALLED_GARDEN_CIDR="10.2.0.0/24"
 
-sudo ufw allow from $WALLED_GARDEN_CIDR any port 8000
-sudo ufw allow from $WALLED_GARDEN_CIDR any port 8002
-sudo ufw allow from $WALLED_GARDEN_CIDR any port 8010
-sudo ufw allow from $WALLED_GARDEN_CIDR any port 8011
-sudo ufw allow from $WALLED_GARDEN_CIDR any port 8013
-sudo ufw allow from $WALLED_GARDEN_CIDR any port 8016
+sudo ufw allow from $WALLED_GARDEN_CIDR to any port 8000
+sudo ufw allow from $WALLED_GARDEN_CIDR to any port 8002
+sudo ufw allow from $WALLED_GARDEN_CIDR to any port 8010
+sudo ufw allow from $WALLED_GARDEN_CIDR to any port 8011
+sudo ufw allow from $WALLED_GARDEN_CIDR to any port 8013
+sudo ufw allow from $WALLED_GARDEN_CIDR to any port 8016
 
 # Allow forwarding (so portal can act as jump host)
 sudo ufw route allow in on $PUBLIC_FACING_IF out on $WALLED_GARDEN_IF
